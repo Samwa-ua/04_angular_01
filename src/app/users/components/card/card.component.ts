4b92ac3b658b75a8fb186ea14a69bd6f3ac4b751
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-06-29 10:01:46 +0300

    feat: Add List of cards

--- src/app/users/components/card/card.component.ts
+++ src/app/users/components/card/card.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IUsers } from 'src/app/users/interfaces/Users';
-import { ApicallService } from 'src/app/users/services/apicall.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent implements OnInit {
-  users: IUsers[] = [];
+export class CardComponent {
+  @Input() users: IUsers;
 
-  constructor(private apiFetchUsers: ApicallService) {}
-
-  ngOnInit() {
-    this.apiFetchUsers.getJSON().subscribe((data) => {
-      this.users = data;
-    });
+  constructor() {
+    this.users = {
+      id: 0,
+      firstname: '',
+      lastname: '',
+      email: '',
+      phone: '',
+    };
   }
 }
--- src/app/users/pages/users-page/users-page.component.ts
+++ src/app/users/pages/users-page/users-page.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IUsers } from '../../interfaces/Users';
+import { ApicallService } from '../../services/apicall.service';
 
 @Component({
   selector: 'app-users-page',
   templateUrl: './users-page.component.html',
-  styleUrls: ['./users-page.component.scss']
+  styleUrls: ['./users-page.component.scss'],
 })
 export class UsersPageComponent implements OnInit {
+  users: IUsers[] = [];
 
-  constructor() { }
+  constructor(private apiFetchUsers: ApicallService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.apiFetchUsers.getJSON().subscribe((data) => {
+      this.users = data;
+      console.log(data);
+    });
   }
-
 }
