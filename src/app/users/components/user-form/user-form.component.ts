b125a7b05eaae8f7f5755751687d8f10823572c7
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-06-30 18:07:16 +0300

    ref: Clear excessive code

--- src/app/users/components/user-form/user-form.component.ts
+++ src/app/users/components/user-form/user-form.component.ts
@@ -1,30 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import {
-  AsyncValidator,
-  FormArray,
-  FormControl,
-  FormGroupDirective,
-  NgForm,
-  Validators,
-} from '@angular/forms';
+import { Validators } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
-import { IEmailValidation } from '../../interfaces/EmailValidation';
-import { IUsers } from '../../interfaces/Users';
 
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(
-    control: FormControl | null,
-    form: FormGroupDirective | NgForm | null
-  ): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(
-      control &&
-      control.invalid &&
-      (control.dirty || control.touched || isSubmitted)
-    );
-  }
-}
+import { IUsers } from '../../interfaces/Users';
 
 @Component({
   selector: 'app-user-form',
@@ -36,38 +14,19 @@ export class UserFormComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {}
 
-  matcher = new MyErrorStateMatcher();
-
   profileForm = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    email: [
-      '',
-      [Validators.required, Validators.email],
-      this.restrictedEmail.bind(this) as unknown as AsyncValidator,
-    ],
+    firstname: ['', Validators.required],
+    lastname: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
     phone: ['', Validators.required],
   });
 
   ngOnInit(): void {}
 
-  restrictedEmail(control: FormControl): Promise<IEmailValidation> {
-    const promise = new Promise<IEmailValidation>((resolve, reject) => {
-      setTimeout(() => {
-        if (control.value === 'test@test.test') {
-          resolve({ restrictedEmail: true });
-        } else {
-          resolve(null!);
-        }
-      }, 2000);
-    });
-    return promise;
-  }
-
   convertToJSON() {
     const data = {
-      firstName: this.profileForm.value.firstName,
-      lastName: this.profileForm.value.lastName,
+      firstName: this.profileForm.value.firstname,
+      lastName: this.profileForm.value.lastname,
       email: this.profileForm.value.email,
       phone: this.profileForm.value.phone,
     };
--- src/app/users/interfaces/EmailValidation.ts
+++
@@ -1,3 +0,0 @@
-export interface IEmailValidation {
-  restrictedEmail?: boolean;
-}
