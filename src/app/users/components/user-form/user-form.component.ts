e2d82c2659c4d4058370a33abc7bd917c7303adb
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-07-02 14:08:18 +0300

    feat: Add required validation to all inputs

--- src/app/users/components/user-form/user-form.component.ts
+++ src/app/users/components/user-form/user-form.component.ts
@@ -15,16 +15,20 @@ export class UserFormComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   profileForm = this.fb.group({
-    firstname: ['', Validators.required],
-    lastname: ['', Validators.required],
+    firstname: ['', [Validators.required, Validators.minLength(2)]],
+    lastname: [
+      '',
+      [Validators.required, Validators.minLength(2), Validators.maxLength(60)],
+    ],
     email: ['', [Validators.required, Validators.email]],
-    phone: ['', Validators.required],
+    phone: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
   });
 
   ngOnInit(): void {}
 
   convertToJSON() {
     const data = {
+      id: Math.floor(Math.random() * 10 + 10),
       firstName: this.profileForm.value.firstname,
       lastName: this.profileForm.value.lastname,
       email: this.profileForm.value.email,
